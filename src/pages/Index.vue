a84d64179e2b0c65f25fc7bcd3869707937095da
abdelouahed <abdelwahd.oumouss@gmail.com>
2021-12-01 19:32:54 +0100

    update(index.vue): get 10 posts per page instead of 1

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,7 +12,7 @@
 
 <page-query>
 query($page: Int) {
-  posts: allPost(filter: { published: { eq: true }}, perPage: 1, page: $page) @paginate {
+  posts: allPost(filter: { published: { eq: true }}, perPage: 10, page: $page) @paginate {
     pageInfo {
       totalPages
       currentPage
