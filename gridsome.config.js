44449549a7be2e30a63bd6bac1a6107ca5c3793f
abdelouahed oumoussa <doonabdo98@gmail.com>
2021-12-20 14:42:27 +0100

    fix(typo): blog name

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'letters',
+  siteName: 'leeters',
   siteDescription: 'A simple, hackable & minimalistic blog for 42 setwok students',
 
   templates: {
